9abe771e8fe765ae75d443e673351df064984e75
MynnTapp <Desmynnmjc@gmail.com>
2024-10-03 18:43:40 -0500

    Andrew's turn

--- src/components/SelectedCoffeeBean.jsx
+++ src/components/SelectedCoffeeBean.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
+
 const SelectedCoffeeBean = () => {
+  const { coffeeBean } = useContext(CoffeeContext);
   return (
     <div className="selected-coffee">
-      <h2>Current Selection: </h2>
+      <h2>Current Selection: {coffeeBean.name}</h2>
     </div>
   );
-}
+};
 
 export default SelectedCoffeeBean;
